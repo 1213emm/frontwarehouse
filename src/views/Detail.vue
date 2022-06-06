1f34f76ab4c5b46b1a2d3f96d8703290f4dd4eca
20373352 <3339267546@qq.com>
2022-06-06 17:12:36 +0800

    DatailAndSearch

---
+++ src/views/Detail.vue
@@ -0,0 +1,144 @@
+<template>
+  <el-container id="detail">
+    <el-header id="head">
+      <el-row style="float: right">
+        欢迎，“已登录用户”！
+        <el-button type="primary" @click="personal">个人空间</el-button>
+      </el-row>
+    </el-header>
+    <el-main id="main">
+        <div id="top">
+          <span style="float:left">
+            <el-button type="danger" icon="el-icon-back" @click="returnSearch">返回</el-button>
+          </span>
+          <span style="float:left" id="author">作者：123</span>
+          <span style="float:left" id="postDate">2022.01.01</span>
+        </div>
+        <div id="post">
+          <el-card>
+            <div slot="header" id="postHead">  
+              <h1 id="title">标题</h1>
+            </div>
+            <div>内容内容内容内容内容</div>
+          </el-card>
+        </div>
+        <div id="actions">
+          <el-button class="act" icon="el-icon-warning" type="danger" circle></el-button>
+          <el-button class="act" icon="el-icon-chat-line-round" type="info" circle></el-button>
+          <el-button class="act" icon="el-icon-star-off" type="warning" circle></el-button>
+          <el-button class="act" icon="el-icon-circle-check" type="primary" circle></el-button>
+        </div>
+        <div id="comments">
+          <div class="floor">
+            <div class="speaker">
+              <span style="float:left;margin-top:10px">F2:</span>
+              <span style="float:left;margin-top:10px">评论人</span>
+              <el-button class="act1" icon="el-icon-circle-check" type="primary" circle></el-button>
+              <el-button class="act1" icon="el-icon-warning" type="danger" circle></el-button>
+            </div>
+            <div class="say"><el-card>你好</el-card></div>
+          </div>
+          <div class="floor">
+            <div class="speaker">
+              <span style="float:left;margin-top:10px">F3:</span>
+              <span style="float:left;margin-top:10px">评论人</span>
+              <el-button class="act1" icon="el-icon-circle-check" type="primary" circle></el-button>
+              <el-button class="act1" icon="el-icon-warning" type="danger" circle></el-button>
+            </div>
+            <div class="say"><el-card>你好啊红红火火恍恍惚惚或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或红红火火恍恍惚惚或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或红红火火恍恍惚惚或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或或</el-card></div>
+          </div>
+        </div>
+    </el-main>
+  </el-container>
+</template>
+
+<!--
+<style>
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+</style>
+-->
+
+<style scoped>
+#detail {
+  font-family: 'Noto Serif SC', serif;
+  margin-top: 20px;
+}
+#head {
+  background-color: #d4e7d9;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+#main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+
+#top {
+  height:60px;
+}
+#author {
+  font-size: 120%;
+  margin-left: 20px;
+  margin-top: 10px;
+}
+#postDate {
+  font-size: 120%;
+  margin-left: 20px;
+  margin-top: 10px;
+}
+
+#post {
+}
+#title {
+  font-size: 200%;
+}
+
+#actions {
+  padding-top: 10px;
+  height: 46.2px
+}
+.act {
+  float: right;
+  font-size :120%;
+  margin-left: 20px;
+}
+
+#comments {
+}
+.floor {
+  margin-top: 10px;
+}
+.speaker {
+  font-size: 110%;
+  height:40px;
+  padding-right:10px;
+  padding-top:10px;
+}
+.say {
+  padding-top:10px;
+}
+.act1 {
+  float: left;
+  margin-left: 10px;
+}
+</style>
+
+<script>
+export default{
+  methods:{
+    personal: function(){
+      this.$router.push('/');
+    },
+    returnSearch: function(){
+      this.$router.push('/search');
+    },
+  }
+}
+</script>
---
+++ src/views/Search.vue
@@ -0,0 +1,109 @@
+<template>
+  <el-container id="search">
+    <el-header id="head">
+      <el-row style="float: right">
+        欢迎，“已登录用户”！
+        <el-button type="primary" @click="personal">个人空间</el-button>
+      </el-row>
+    </el-header>
+    <el-main id="main">
+      <div class="search" @click="search">
+        <el-input placeholder="请输入内容" v-model="input" clearable>
+          <el-button type="primary" icon="el-icon-search" slot="append">搜索</el-button>
+        </el-input>
+      </div>
+      <div>
+        <h3 style="float: left">为您推荐：</h3>
+      </div>
+      <div>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column type="index"> </el-table-column>
+          <el-table-column prop="title" label="标题"></el-table-column>
+          <el-table-column prop="name" label="作者"></el-table-column>
+          <el-table-column prop="date" label="日期"></el-table-column>
+          <el-table-column prop="likes" label="点赞数"></el-table-column>
+          <el-table-column>
+            <template slot-scope="scope">
+              <el-link type="primary" @click="seeblog(scope.id)">查看详情（未完成）</el-link>
+            </template>
+          </el-table-column>
+          <el-table-column>
+            <template>
+              <el-link type="primary" href="http://localhost:8080/detail">test to detail</el-link>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import qs from "qs";
+export default {
+  data() {
+    return {
+      input: "",
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          title: "上海市普陀区金沙江路 1518 弄",
+          likes: 10,
+        },
+        {
+          date: "2016-05-04",
+          name: "王小",
+          title: "期末考试",
+          likes: 15,
+        },
+      ],
+    };
+  },
+  methods:{
+    personal: function(){
+      this.$router.push('/');
+    },
+    search: function(){
+      if (!$store.state.islogin) {
+        this.$router.push("/Login");
+      } else {
+        this.$axios({
+          method: "post" /* 指明请求方式，可以是 get 或 post */,
+          url: "/info" /* 指明后端 api 路径，由于在 main.js 已指定根路径，因此在此处只需写相对路由 */,
+          data: qs.stringify({
+            /* 需要向后端传输的数据，此处使用 qs.stringify 将 json 数据序列化以发送后端 */
+            input: this.input,
+          }),
+        }).then((res) => {
+          switch (res.data.errno) {
+            case 0:
+              break;
+            case 100:
+              this.$message.error("无相关内容");
+              break;
+          }
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+#search {
+  font-family: 'Noto Serif SC', serif;
+  margin-top: 20px;
+}
+#head {
+  background-color: #d4e7d9;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+#main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+</style>
